b5459246aef10dea5cf3eaa1e3136b2e20f20002
Brian Anglin <brianranglin+gh@gmail.com>
2017-09-15 19:18:14 -0700

    Adds nock

--- tests/unit/skills/echo.js
+++ tests/unit/skills/echo.js
@@ -1,13 +1,16 @@
-const request = require('supertest')
+const request = require('supertest');
 const Slapp = require('slapp')
 const express = require('express')
 const { meta, context } = require('../helpers')
 const echo = require('../../../src/skills/echo')
 const direct_message = require('../../fixtures/direct_message')
+const nock = require('nock')
+
 
 describe('echo', () => {
   let slapp
   let app
+  nock.disableNetConnect()
   beforeEach(() => {
     slapp = Slapp({ context })
     app = express()
@@ -17,10 +20,19 @@ describe('echo', () => {
 
   describe('without text to echo', () => {
     it.only('should respond with help text', async () => {
+      nock(/.*/)
+        .post(/.*/, (body) => {
+          console.log('BODYFDSO', body) 
+          return true;
+        })
+        .reply(200)
+
       let resp = await request(app)
         .post('/slack/event').send(direct_message)
 
-      // console.log('resp', resp)
+      //console.log('resp', resp)
     })
   })
 })
+
+
