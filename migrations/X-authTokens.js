5a9f3d0251c4d096cf79f6f48f9817a65066b1a2
Ritvik <ritviksalim@gmail.com>
2017-11-09 14:15:40 -0800

    migration file for authTokens

---
+++ migrations/X-authTokens.js
@@ -0,0 +1,33 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable(
+      'authtokens',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        createdAt: {
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          type: Sequelize.DATE
+        },
+        token: {
+          type: Sequelize.STRING
+        },
+        teamId: {
+          type: Sequelize.STRING
+        }
+      },
+      {
+        schema: 'public'                      // default: public, PostgreSQL only.
+      }
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+      return queryInterface.dropTable('authtokens')
+  }
+}
